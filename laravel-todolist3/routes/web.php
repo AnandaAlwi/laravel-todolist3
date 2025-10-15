5bc214b021bf672e087319ae5deb59fa3e61404a
Ananda Alwi <anandaalwi007@gmail.com>
2025-10-15 15:37:28 +0700

    update routes for task CRUD

--- laravel-todolist3/routes/web.php
+++ laravel-todolist3/routes/web.php
@@ -1,18 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::resource('tasks', TaskController::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('tasks.toggle');
+
+Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+
+Route::get('/tasks/{task}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+
+Route::put('/tasks/{task}', [TaskController::class, 'update'])->name('tasks.update');
+
+Route::delete('/tasks/{task}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+
+Route::patch('/tasks/{task}/toggle', [TaskController::class, 'toggle'])->name('tasks.toggle');
