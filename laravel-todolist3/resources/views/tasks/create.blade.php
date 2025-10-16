a85b874ff858ff6bd9d7a92d3cc2222168ca3988
Ananda Alwi <anandaalwi007@gmail.com>
2025-10-16 10:02:26 +0700

    update index.blade.php: menambahkan kode baru

---
+++ laravel-todolist3/resources/views/tasks/create.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Tambah Tugas Baru</title>
+    <style>
+        * {
+            margin:0;
+            padding:0;
+            box-sizing:border-box;
+        }
+        body {
+            font-family:"Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+            background:linear-gradient(to bottom right,black,gray);
+            padding:40px;
+            background-attachment:fixed;
+        }
+    </style>
+<body>
+    
+</body>
+</html>
